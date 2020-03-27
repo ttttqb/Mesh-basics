0ca2011916174fd37f241cd5dc9b941c15d7116a
ttttqb <tanqinbing@hotmail.com>
2020-03-27 19:16:02 +0800

    Finish tutorial.

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -1,15 +1,22 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshDeformer : MonoBehaviour
 {
+    public float springForce = 20;
+    public float damping = 5f;
+    
     private Mesh deformingMesh;
     private Vector3[] originalVertices, displacedVertices;
     private Vector3[] vertexVelocities;
+    private float uniformScale = 1f;
 
 
-    private void Start () {
+    private void Start ()
+    {
+        uniformScale = transform.localScale.x;
         deformingMesh = GetComponent<MeshFilter>().mesh;
         originalVertices = deformingMesh.vertices;
         displacedVertices = new Vector3[originalVertices.Length];
@@ -19,8 +26,44 @@ public class MeshDeformer : MonoBehaviour
         vertexVelocities= new Vector3[originalVertices.Length];
     }
 
+    private void Update()
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+    }
+
+    private void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        displacement *= uniformScale;
+        velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - damping * Time.deltaTime;
+        vertexVelocities[i] = velocity;
+        displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+    }
+
     public void AddDeformingForce(Vector3 point, float force)
     {
-        Debug.DrawLine(Camera.main.transform.position, point);
+        //Debug.DrawLine(Camera.main.transform.position, point);
+        point = transform.InverseTransformPoint(point);
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    private void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        pointToVertex *= uniformScale;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
     }
 }
